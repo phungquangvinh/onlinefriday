7a3fa278dc3dcd68d7b55562a87e8a92f858b99b
TGS\TGS <v96hoangtucodon@gmail.com>
2019-08-03 12:17:14 +0700

    feat: complete homepage (fixing...)

--- js/slide.js
+++ js/slide.js
@@ -2,5 +2,27 @@ $( document ).ready(function() {
     $('.owl-carousel.banner').owlCarousel({
 	    loop:true,
 	    items: 1,
+	    autoplay:true,
+	    autoplayTimeout:5000,
 	});
 });
+
+$( document ).ready(function () {
+	$('.owl-carousel.item').owlCarousel({
+	    loop:true,
+	    margin:0,
+	    nav:true,
+	    dots:false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:4
+	        },
+	        1000:{
+	            items:8
+	        }
+	    }
+	})
+})
